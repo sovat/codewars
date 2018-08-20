dd23ef62342240fd41761e5523470d7469454e06
sovat <bds@pkzdrav.ru>
2018-08-20 15:34:43 +0500

    close, but don`t working

--- src/MexicanWave.java
+++ src/MexicanWave.java
@@ -7,18 +7,23 @@ public class MexicanWave {
     public static String[] wave(String str) {
         String[] result = new String[str.length()];
         char[] temp = str.toCharArray();
-        if (!(str.indexOf(0) == ' ' || str.indexOf(temp.length) == ' ')) {
+        if (!(str.startsWith(" ")|| str.endsWith(" "))) {
             for (int i = 0; i < temp.length; i++) {
+                if (temp[i] == ' ') {i++; }
+                temp[i] = Character.toUpperCase(temp[i]);
+                result[i] = String.valueOf(temp);
+                temp[i] = Character.toLowerCase(temp[i]);
+            }
+        }
+            for (int i = 0; i < temp.length-1; i++) {
 
                 if (temp[i] == ' ') {
-
                     i++;
                 }
                 temp[i] = Character.toUpperCase(temp[i]);
                 result[i] = String.valueOf(temp);
                 temp[i] = Character.toLowerCase(temp[i]);
             }
-        }
 
         List<String> list = new ArrayList<String>();
         for (String s : result) {
@@ -31,7 +36,7 @@ public class MexicanWave {
 
 
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(wave(" gap ")));
+        System.out.println(Arrays.asList(wave("two words")));
     }
 
 }
