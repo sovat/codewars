e36c6aaf1c10153603df7f8054e58e3d863d8b35
sovat <bds@pkzdrav.ru>
2018-08-31 13:08:20 +0500

    https://www.codewars.com/kata/58f5c63f1e26ecda7e000029

--- src/kyu6/MexicanWave.java
+++ src/kyu6/MexicanWave.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+Task:
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+Rules:
+ 	1.  The input string will always be lower case but maybe empty.
+
+2.  If the character in the string is whitespace then pass over it as if it was an empty seat.
+* */
+
 public class MexicanWave {
 
     public static String[] wave(String str) {
