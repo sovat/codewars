9b2b286217ea2493be0e3a552147ad5ebb322ac4
sovat <bds@pkzdrav.ru>
2018-11-01 17:33:17 +0500

    https://www.codewars.com/kata/abbreviate-a-two-word-name/java Исправить - принимать в цикл вместо массива цикл и удалять из него пройденные элементы.

--- src/kyu5/WeightSort.java
+++ src/kyu5/WeightSort.java
@@ -1,40 +1,46 @@
 package kyu5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WeightSort {
+
+    public static Integer summ(Integer element) {
+        int res = 0;
+        int temp = element;
+        while (temp != 0) {
+            res += temp % 10;
+            temp /= 10;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
+
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
 
         Integer[] arr = {56, 65, 74, 100, 99, 68, 86, 180, 90};
 
-        int temp, res = 0;
-
         for (int i = 0; i < arr.length; i++) {
-            temp = arr[i];
-            while (temp != 0) {
-                res += temp % 10;
-                temp /= 10;
+            ArrayList<Integer> list = new ArrayList();
+            for (int j = i; j < arr.length; j++) {
+                if (summ(arr[i]) == summ(arr[j])) {
+                    list.add(arr[j]);
+
+                }
             }
-            System.out.println(res);
-            res = 0;
+            map.put(summ(arr[i]), list);
+
+
         }
-
-
-
-        /*HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        List
-        map.put(11, );
-        map.put(11, 65);
-        map.put(11, 74);
-        map.put(1, 100);
-        map.put(18, 99);
-        map.put(14, 68);
-        map.put(14, 86);
-        map.put(9, 180);
-        map.put(9, 90);
 
         for (Map.Entry entry : map.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " Value: "
                     + entry.getValue());
-        }*/
+        }
 
     }
 }
