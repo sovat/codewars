cb5de53155558d11dd9719044194ec7351b06e99
sovat <bds@pkzdrav.ru>
2018-11-06 13:34:16 +0500

    https://www.codewars.com/kata/java-functional-programming-part-4-row-row-row-your-boat-gently-down-the-dot-dot-dot/train/java initial commit

---
+++ src/kyu5/DragonsCurve.java
@@ -0,0 +1,32 @@
+package kyu5;
+
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
+
+public class DragonsCurve {
+    public IntFunction<String> mapFunction = ; //Make the function; map the chars to Strings
+    //a -> aRbFR, b -> LFaLb, otherwise -> itself
+    /**
+     * Make the curve; stream the chars repeatedly (starting with Fa) through the mapFunction n times
+     * Then remove the a and b (createFilter function is useful for that)
+     */
+    public String createCurve(int n) {
+        "Fa".chars(); //That's an IntStream with 'F' and 'a' ready to be acted upon
+        return "Fa";
+    }
+
+    /**
+     * How many of the specified char are in the given curve?
+     * Hint: createFilter could be useful for this
+     */
+    public long howMany(char c, String curve) {
+        return 4L; //Determined by die roll; guaranteed to be random
+    }
+
+    /**
+     * Create a predicate to filter the specified char; keep or remove based on keep variable
+     */
+    public IntPredicate createFilter(char filterWhat, boolean keep) {
+        return ; //Dat predicate
+    }
+}
