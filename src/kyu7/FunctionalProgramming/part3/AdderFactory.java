5968c325bf69708365863589cb00cd639408076c
sovat <bds@pkzdrav.ru>
2018-11-06 13:26:17 +0500

    https://www.codewars.com/kata/java-functional-programming-part-3-closured-for-business/train/java

---
+++ src/kyu7/FunctionalProgramming/part3/AdderFactory.java
@@ -0,0 +1,14 @@
+package kyu7.FunctionalProgramming.part3;
+
+import java.util.function.IntUnaryOperator;
+
+public class AdderFactory {
+    /*
+     * Make the "create" method (public, static). It accepts one parameter (int addTo) and returns
+     * a function (make sure to use the appropriate type). This function accepts an integer,
+     * adds "addTo" to that integer, and returns the result as an integer (integer-to-integer function).
+     */
+    public static IntUnaryOperator create(int addTo) {
+        return  x-> x + addTo;
+    }
+}
