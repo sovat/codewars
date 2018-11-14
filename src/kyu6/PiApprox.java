f871fb1d83a02266b6fc5ec060b9b52924c0af07
sovat <bds@pkzdrav.ru>
2018-11-14 11:43:11 +0500

    https://www.codewars.com/kata/550527b108b86f700000073f

---
+++ src/kyu6/PiApprox.java
@@ -0,0 +1,23 @@
+package kyu6;
+
+import java.math.BigDecimal;
+
+public class PiApprox {
+    public static String iterPi2String(Double epsilon) {
+        int n = 1;
+        double pi = 4.;
+        double factor = -4.;
+        while (Math.abs(Math.PI - pi) > epsilon) {
+            pi += factor / (n * 2 + 1);
+            n++;
+            factor = -factor;
+        }
+        return new String("["  + n + ", " + BigDecimal.valueOf(pi).setScale(10, BigDecimal.ROUND_HALF_UP) + "]");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(iterPi2String(0.1));
+        System.out.println(Integer.parseInt("1"));
+    }
+
+}
